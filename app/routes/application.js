abda78d5e43a05505620e81d6cc296eee518e662
santuan <smrcouto88@gmail.com>
2015-09-14 18:31:55 -0300

    se agregaron librerias basicas

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
+import GoBackMixin from 'ember-smart-go-back/mixins/application-route';
 
-export default Ember.Route.extend(ApplicationRouteMixin, {
+export default Ember.Route.extend(ApplicationRouteMixin, GoBackMixin, {
 });
+
