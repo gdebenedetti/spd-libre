71cbbd2bf9e69e0bd7dd0be9b6fa02bc33f74b4c
Mbiondo <mbiondo@goblab.org>
2015-09-17 19:18:53 -0300

    Paginador fixed

--- app/routes/ordenes-del-dia.js
+++ app/routes/ordenes-del-dia.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
-import InfinityRoute from "../../mixins/infinity-route";
+import InfinityRoute from "../mixins/infinity-route";
 
 export default Ember.Route.extend(InfinityRoute, AuthenticatedRouteMixin, {
 	_listName: 'model',
