0eb4711658357dd1b1eb76382850fab73eaa2a4a
santuan <smrcouto88@gmail.com>
2015-09-24 18:18:37 -0300

    se creo el panel de documentos

--- app/transitions.js
+++ app/transitions.js
@@ -3,22 +3,43 @@ export default function(){
 	this.transition(
 		this.fromRoute('index'),
 		this.toRoute('login'),
-		this.use('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
-		this.reverse('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+		this.use('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
 	);
 
 	this.transition(
 		this.fromRoute('index'),
 		this.toRoute('proyectos.index'),
-		this.use('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
-		this.reverse('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+		this.use('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+	this.transition(
+		this.fromRoute('index'),
+		this.toRoute('comisiones.index'),
+		this.use('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
 	);
 
 	this.transition(
 		this.fromRoute('proyectos.index'),
 		this.toRoute('proyectos.show'),
-		this.use('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
-		this.reverse('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+		this.use('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+	this.transition(
+		this.fromRoute('comisiones.index'),
+		this.toRoute('comisiones.show'),
+		this.use('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	);
+
+	this.transition(
+		this.fromRoute('proyectos.show'),
+		this.toRoute('comisiones.show'),
+		this.use('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
 	);
 
 	this.transition(
@@ -27,4 +48,14 @@ export default function(){
 		this.use('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
 		this.reverse('fade',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
 	);	
+
+	this.transition(
+		this.hasClass('side-panel'),
+		// this makes our rule apply when the liquid-if transitions to the
+		// true state.
+		this.toValue(true),
+		this.use('toLeft',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]}),
+		this.reverse('toRight',  {duration: 800, delay: 400, easing: [ 0.7,0,0.3,1 ]})
+	  );
+	
 };
